a91b269a657d4f6b1fdf0a1d0e12890b86826380
Sudipta Bose <sudiptabose96@gmail.com>
2023-08-07 03:07:21 +0530

    first commit

---
+++ traverse.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class traverse {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int arrSize = 0;
+        System.out.println("Enter the size of the array");
+
+        if (sc.hasNextInt()) {
+            arrSize = sc.nextInt();
+        }
+        int[] arr = new int[arrSize];
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < arrSize; i++) {
+            if (sc.hasNextInt()) {
+                arr[i] = sc.nextInt();
+
+            }
+        }
+        System.out.print("Traverse of an array:");
+        for (int i = 0; i < arrSize; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        sc.close();
+    }
+}
